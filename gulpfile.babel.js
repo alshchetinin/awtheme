799209a20beb97061c8d76aaab18eb40734d1491
alshchetinin <alexsander.qw@gmail.com>
2021-01-11 13:18:40 +0300

    first-commite

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -86,7 +86,7 @@ export const watchForChanges = () => {
 const server = browserSync.create();
 export const serve = done => {
   server.init({
-    proxy: "http://domus.local" // put your local website link here
+    proxy: "http://awtheme.local/" // put your local website link here
   });
   done();
 };
